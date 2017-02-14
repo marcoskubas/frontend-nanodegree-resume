f7bd383aa56041415729b1e6cf023d8c1f0e2666
Marcos Kubaszewski <marcosarobed@gmail.com>
2017-02-13 22:10:28 -0200

    Dot Notation e Object Literal Notation

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -18,6 +18,42 @@ var name = 'Marcos Kubas';
 var formattedName = HTMLheaderName.replace("%data%", name);
 $('#header').prepend(formattedName);
 
+var skills = ['PHP', 'Javascript', 'Python'];
+
+var bio = {
+    "name" : "Marcos Kubas",
+    "role" : "Web Developer",
+    "contacts" : {
+        "mobile" : "+55 51 99929.7876",
+        "email" : "marcosarobed@gmail.com",
+        "github" : "https://github.com/marcoskubas",
+        "twitter" : "https://twitter.com/marcoskubas",
+        "location" : "Brazil"
+    },
+    "picture_url" : "https://media.licdn.com/media/p/2/005/009/3ad/1d8cfcb.jpg",
+    "welcome_message" : "Lorem Ipsun bla, bla, bla",
+    "skills" : skills
+}
+
+bio['city'] = "Guaíba/RS";
+
+// $("#main").append(bio.name);
+
+$("#main").append(bio['city']);
+
+var work = {}
+work.position = "Web developer";
+work.employer = "Ruá start";
+work.years    = "Seven years";
+
+var education = {};
+education["school"] = 'Ulbra';
+education["years"]  = "Nine years"
+education["city"]   = "Guaíba/RS"
+
+$("#main").append(work['position']);
+$("#main").append(education.school);
+
 /*var sampleArray = [0,0,7];
 
 var incrementLastArrayElement = function(_array) {
